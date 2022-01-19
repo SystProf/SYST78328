39b143858303f787f4a423c4a217931cc44870d7
SystProf <97409223+SystProf@users.noreply.github.com>
2022-01-19 13:16:23 -0500

    remove unwanted comments

--- src/studlist/Student.java
+++ src/studlist/Student.java
@@ -1,26 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package studlist;
 
 /**
  *
- * @author Megha Patel
+ * @author Megha Patel, 2022
  */
 public class Student {
     private String name;
     
-//    public void setName(String name)
-//    {
-//        this.name=name;
-//    }
-//    
-//    public String getName()
-//    {
-//        return name;
-//    }
 
     /**
      * @return the name
